b600128589a7ff78abd1ce935486ad0f45acf6d8
jlvFlores <jluisvazquez.jv@gmail.com>
2022-09-19 18:55:55 -0500

    1_12 cambiar las animaciones segun los estados

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,42 +7,48 @@ public class PlayerController : MonoBehaviour
     //Variables del movimiento
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
+    Animator animator;
+
+    private const string STATE_ALIVE = "isAlive";
+    private const string STATE_ON_GROUND = "isOnGround";
 
     public LayerMask groundMask;
     void Awake ()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_GROUND, false);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             Jump();
         }
 
+        animator.SetBool(STATE_ON_GROUND, IsTouchingGround());
+
         Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
     }
 
-    void Jump()
-    {
-        if(isTouchingGround()){
-            rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+    void Jump() {
+        if(IsTouchingGround()){
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    bool isTouchingGround() {
+    //comprueba si el jugador esta tocando el suelo
+    bool IsTouchingGround() {
         if(Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask)) {
-            //TODO: programar logica de contacto con el suelo 
+            animator.enabled = true;
             return true;
         } else {
-            //TODO: programar logica de no contacto
+            animator.enabled = false;
             return false;
         }
     }
