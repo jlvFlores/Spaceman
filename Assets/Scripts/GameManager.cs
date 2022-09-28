0632e3fb30803600bfae29f79d6d77903a0f9f5d
jlvFlores <jluisvazquez.jv@gmail.com>
2022-09-27 19:12:48 -0500

    2_1 Como funciona el Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState{
+	menu,
+	inGame,
+	gameOver
+}
+
+public class GameManager : MonoBehaviour {
+
+	public GameState currentGameState = GameState.menu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+
+	}
+
+	public void GameOver(){
+
+	}
+
+	public void BackToMenu(){
+
+	}
+
+	private void SetGameState(GameState newGameState){
+		if(newGameState == GameState.menu){
+			//TODO: colocar la logica del menú
+		} else if(newGameState == GameState.inGame){
+			//TODO: hay que preparar la escena para jugar
+		} else if(newGameState == GameState.gameOver){
+			//TODO: preparar el juego para el Game Over
+		}
+
+
+		this.currentGameState = newGameState;
+	}
+    
+}
