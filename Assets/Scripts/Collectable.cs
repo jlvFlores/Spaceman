44a3a1a10e555d7803a39ed7aa74e84d8b497737
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-26 19:05:23 -0500

    4_6 Los coleccionables del juego

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollectableType{
+    healthPotion,
+    manaPotion,
+    money
+}
+
+public class Collectable : MonoBehaviour
+{
+
+    public CollectableType type = CollectableType.money;
+
+    private SpriteRenderer sprite;
+    private CircleCollider2D itemCollider;
+
+    bool hasBeenCollected = false;
+
+    public int value = 1;
+
+    void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        itemCollider = GetComponent<CircleCollider2D>();    
+    }
+
+    void Show(){
+        sprite.enabled = true;
+        itemCollider.enabled = true;
+        hasBeenCollected = false;
+    }
+
+    void Hide(){
+        sprite.enabled = false;
+        itemCollider.enabled = false;
+    }
+
+    void Collect(){
+        Hide();
+        hasBeenCollected = true;
+
+        switch(this.type){
+            case CollectableType.money:
+                //TODO: logica de moneda
+                break;
+            case CollectableType.healthPotion:
+                //TODO: logica de pocion de vida
+                break;
+            case CollectableType.manaPotion:
+                //TODO: logica de pocion de mana
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            Collect();
+        }
+    }
+}
